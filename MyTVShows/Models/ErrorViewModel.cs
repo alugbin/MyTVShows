ab3391f308302240f919856d6647c3d622ef02ab
Adekunle <aalugbin@sbsc.com>
2020-03-13 19:03:58 +0100

    Add project files.

---
+++ MyTVShows/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyTVShows.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
